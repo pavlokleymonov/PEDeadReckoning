158bd5a60bfaf7303f3b1d670d59b321a479f04f
Pavlo Kleymonov <pavlo.kleymonov@gmail.com>
2017-05-29 14:38:17 +0200

    Added more unittests for class calibration ex

--- calibration/test/ut/PECalibrationExTest.cpp
+++ calibration/test/ut/PECalibrationExTest.cpp
@@ -14,7 +14,7 @@
 
 
 /**
- * Unit test of the PECalibrationTest class.
+ * Unit test of the PECalibrationEx class.
  *
  * Code under test:
  *
@@ -27,6 +27,8 @@
 
 using ::testing::_;
 using ::testing::Eq;
+using ::testing::DoubleEq;
+using ::testing::DoubleNear;
 using ::testing::ByRef;
 
 
@@ -39,32 +41,97 @@ public:
    {}
 };
 
+
+/**
+ * tests reference value above accuracy limit inside incoming data
+ * callback check
+ */
+TEST_F(PECalibrationExTest, ref_above_limit_in_between_CB)
+{
+   PE::Mock_normalisation mock;
+
+   PE::calibration_ex calib(mock,10); //10 meters accuracy
+
+   //start accumulating of data
+   calib.add_sensor(100);
+   calib.add_reference(100,5); //added 100 meters with accuracy +/-5meters 
+   calib.add_sensor(100);
+   calib.add_reference(100,11); //added 100 meters with accuracy above limit
+   //resets all accumulated data
+   calib.add_sensor(100);
+   calib.add_reference(100,5); //added 100 meters with accuracy +/-5meters 
+   //starts accumulating new data
+   calib.add_sensor(400);
+   calib.add_reference(60,5); //added 100 meters with accuracy +/-5meters 
+   calib.add_sensor(600);
+   //Expecte call has to be set before real call
+   PE::TValue ratio = (400+600)/(60+40);
+   EXPECT_CALL(mock,add_sensor(Eq(ByRef(ratio))));
+   calib.add_reference(40,5); //added 100 meters with accuracy +/-5meters 
+}
+
+
+/**
+ * tests ignore all references values till first sensor data
+ * callback check
+ */
+TEST_F(PECalibrationExTest, ignore_unused_ref_data_CB)
+{
+   PE::Mock_normalisation mock;
+
+   PE::calibration_ex calib(mock,10); //10 meters accuracy
+
+   //start accumulating of data
+   calib.add_reference(100,5); //added 100 meters with accuracy +/-5meters 
+   calib.add_reference(100,5); //added 100 meters with accuracy +/-5meters 
+   calib.add_reference(100,5); //added 100 meters with accuracy +/-5meters 
+   calib.add_reference(100,5); //added 100 meters with accuracy +/-5meters 
+   //all reference values have to be ignored since no sensordata so far.
+   calib.add_sensor(100);
+   calib.add_reference(10,5); //added 10 meters with accuracy +/-5meters 
+   calib.add_sensor(200);
+   calib.add_reference(20,5); //added 20 meters with accuracy +/-5meters 
+   calib.add_sensor(300);
+   //Expecte call has to be set before real call
+   PE::TValue ratio = (100+200+300)/(10+20+30);
+   EXPECT_CALL(mock,add_sensor(Eq(ByRef(ratio))));
+   calib.add_reference(30,5); //added 30 meters with accuracy +/-5meters 
+}
+
+
 /**
  * tests 10 sensors 3 valid reference 2 invalid references
  * callback check
  */
 TEST_F(PECalibrationExTest, _10_sensors_3_valid_2_invalid_ref_test_CB)
 {
-//TODO
+
    PE::Mock_normalisation mock;
 
    PE::calibration_ex calib(mock,10); //10 meters accuracy
 
    calib.add_sensor(1000); //has to be ignored before first valid ref
    calib.add_sensor(1000); //has to be ignored before first valid ref
-
    calib.add_reference(100,5); //added 100 meters with accuracy +/-5meters 
    calib.add_sensor(500);
    calib.add_sensor(500);
+   calib.add_sensor(500);
+   calib.add_reference(100,11); //added 100 meters with accuracy above the limit
+   calib.add_sensor(5000);
+   calib.add_sensor(5000);
+   //till now all sensors has to be ignored
    calib.add_reference(100,5); //added 100 meters with accuracy +/-5meters 
    calib.add_sensor(500);
-   calib.add_sensor(500);
+   calib.add_sensor(300);
+   calib.add_sensor(200);
+   calib.add_reference(10,5); //added 100 meters with accuracy +/-5meters 
+   calib.add_sensor(100);
+   calib.add_sensor(100);
+   calib.add_sensor(100);
    //Expecte call has to be set before real call
-   PE::TValue ratio = (500+500+500+500)/(100+100);
+   PE::TValue ratio = (500+300+200+100+100+100)/(10+3);
    EXPECT_CALL(mock,add_sensor(Eq(ByRef(ratio))));
-   calib.add_reference(100,5); //added 100 meters with accuracy +/-5meters 
-   calib.add_sensor(500);
-   calib.add_sensor(500);
+   calib.add_reference(3,5); //added 100 meters with accuracy +/-5meters 
 }
 
 
