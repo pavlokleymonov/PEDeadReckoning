4ba4c75f019b8533004185b6b542bccea47e8914
Pavlo Kleymonov <pavlo.kleymonov@gmail.com>
2018-08-20 15:49:41 +0200

    Extend core simple coverage

--- test/ut/PECCoreSimpleTest.cpp
+++ test/ut/PECCoreSimpleTest.cpp
@@ -39,6 +39,78 @@ public:
 TEST_F(PECCoreSimpleTest, create_test )
 {
    PE::CCoreSimple core(PE::SPosition()/* invalid position */, PE::SBasicSensor()/* invalid heading */, 0.100/*100ms*/);
+   EXPECT_FALSE(core.GetOdoCfg().IsValid());
+   EXPECT_FALSE(core.GetGyroCfg().IsValid());
+   EXPECT_FALSE(core.GetPosition().IsValid());
+   EXPECT_FALSE(core.GetHeading().IsValid());
+   EXPECT_FALSE(core.GetSpeed().IsValid());
+}
+
+
+/**
+ * test
+ */
+TEST_F(PECCoreSimpleTest, no_valid_sensors_cfg_test )
+{
+   PE::CCoreSimple core(PE::SPosition()/* invalid position */, PE::SBasicSensor()/* invalid heading */, 0.100/*100ms*/);
+
+   //no set configuration
+   EXPECT_FALSE(core.GetOdoCfg().IsValid());
+   EXPECT_FALSE(core.GetGyroCfg().IsValid());
+
+   //set invalid config
+   core.SetOdoCfg(PE::CSensorCfg(),2,1);
+   core.SetGyroCfg(PE::CSensorCfg(),2,1);
+   EXPECT_FALSE(core.GetOdoCfg().IsValid());
+   EXPECT_FALSE(core.GetGyroCfg().IsValid());
+
+   core.AddOdo(1.000, PE::SBasicSensor(1000, 0.1));
+   core.AddGyro(1.001, PE::SBasicSensor(1, 0.1));
+   core.AddOdo(2.000, PE::SBasicSensor(2000, 0.1));
+   core.AddGyro(2.001, PE::SBasicSensor(2, 0.1));
+   core.AddOdo(3.000, PE::SBasicSensor(3000, 0.1));
+   core.AddGyro(3.001, PE::SBasicSensor(3, 0.1));
+
+   EXPECT_FALSE(core.GetPosition().IsValid());
+   EXPECT_FALSE(core.GetHeading().IsValid());
+   EXPECT_FALSE(core.GetSpeed().IsValid());
+}
+
+
+/**
+ * test
+ */
+TEST_F(PECCoreSimpleTest, valid_odo_cfg_test )
+{
+   PE::CCoreSimple core(PE::SPosition()/* invalid position */, PE::SBasicSensor()/* invalid heading */, 0.100/*100ms*/);
+
+   core.SetOdoCfg(PE::CSensorCfg(PE::CSensorCfg::ToCFG("CFGSENSOR,5,1.00000000,0.00000000,100.0,1,0.00000000,0.00000000,100.0,1,99.5,XX")), 2, 1); //SENSOR_ODOMETER_AXIS = 5
+   EXPECT_TRUE(core.GetOdoCfg().IsValid());
+   EXPECT_EQ(99.5, core.GetOdoCfg().GetLimit());
+
+   //rewrite new config
+   core.SetOdoCfg(PE::CSensorCfg(PE::CSensorCfg::ToCFG("CFGSENSOR,5,1.00000000,0.00000000,100.0,1,0.00000000,0.00000000,100.0,1,90.0,XX")), 2, 1); //SENSOR_ODOMETER_AXIS = 5
+   EXPECT_TRUE(core.GetOdoCfg().IsValid());
+   EXPECT_EQ(90.0, core.GetOdoCfg().GetLimit());
+
+}
+
+
+/**
+ * test
+ */
+TEST_F(PECCoreSimpleTest, valid_gyro_cfg_test )
+{
+   PE::CCoreSimple core(PE::SPosition()/* invalid position */, PE::SBasicSensor()/* invalid heading */, 0.100/*100ms*/);
+
+   core.SetGyroCfg(PE::CSensorCfg(PE::CSensorCfg::ToCFG("CFGSENSOR,6,1.00000000,0.00000000,100.0,1,0.00000000,0.00000000,100.0,1,99.5,XX")), 2, 1); //SENSOR_GYRO_Z = 6
+   EXPECT_TRUE(core.GetGyroCfg().IsValid());
+   EXPECT_EQ(99.5, core.GetOdoCfg().GetLimit());
+
+   //rewrite new config
+   core.SetGyroCfg(PE::CSensorCfg(PE::CSensorCfg::ToCFG("CFGSENSOR,6,1.00000000,0.00000000,100.0,1,0.00000000,0.00000000,100.0,1,90.1,XX")), 2, 1); //SENSOR_GYRO_Z = 6
+   EXPECT_TRUE(core.GetGyroCfg().IsValid());
+   EXPECT_EQ(90.1, core.GetGyroCfg().GetLimit());
 }
 
 
