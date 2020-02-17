03f1412e6bd0176904c3dee74633f704cccadd58
Pavlo Kleymonov <pavlo.kleymonov@gmail.com>
2020-02-17 16:22:04 +0100

    Updated UT for PECOdometerEx

--- test/ut/PECOdometerExTest.cpp
+++ test/ut/PECOdometerExTest.cpp
@@ -146,9 +146,77 @@ TEST_F(PECOdometerExTest, test_set_get_reference_value)
    //init values have to be NaN
    EXPECT_TRUE( PE::isnan(odo.GetRefValue()) );
 
-   //simulate first ref value, oldTimestamp is 0
-   EXPECT_FALSE( odo.SetRefValue(0, 1.000, 10.0, 0.1) );
+   //check value range
+   EXPECT_TRUE ( odo.SetRefValue(1.000, 1.100, SPEED_MAX, 0.1) );
+   EXPECT_TRUE ( odo.SetRefValue(1.000, 1.100, SPEED_MIN, 0.1) );
+   EXPECT_FALSE( odo.SetRefValue(1.000, 1.100, SPEED_MIN-1, 0.1) );
+   EXPECT_FALSE( odo.SetRefValue(1.000, 1.100, SPEED_MAX+1, 0.1) );
 
+   //check intervals
+   EXPECT_TRUE ( odo.SetRefValue(1.000, 1.100, SPEED_MAX, 0.1) );
+   EXPECT_TRUE ( odo.SetRefValue(1.000, 1.109, SPEED_MAX, 0.1) );
+   EXPECT_TRUE ( odo.SetRefValue(1.000, 1.091, SPEED_MAX, 0.1) );
+   EXPECT_FALSE( odo.SetRefValue(1.000, 1.111, SPEED_MAX, 0.1) );
+   EXPECT_FALSE( odo.SetRefValue(1.000, 1.089, SPEED_MAX, 0.1) );
+
+   //checks accuracy
+   EXPECT_TRUE ( odo.SetRefValue(1.000, 1.100, 10.1, 5.0) );
+   EXPECT_FALSE( PE::isnan(odo.GetRefValue()) );
+   
+   EXPECT_TRUE ( odo.SetRefValue(1.000, 1.100,  9.9, 5.0) );
+   EXPECT_TRUE( PE::isnan(odo.GetRefValue()) );
+}
+
+
+/**
+ * test set/get sensor value
+ */
+TEST_F(PECOdometerExTest, test_set_get_sensor_value)
+{
+   PE::TValue SPEED_INTERVAL_100MS           = 0.100;
+   PE::TValue SPEED_INTERVAL_HYSTERESIS_10MS = 0.010;
+   PE::TValue SPEED_MIN                      = 0.0;
+   PE::TValue SPEED_MAX                      = 343.0; //sound speed ~1235km/h
+   PE::TValue SPEED_ACCURACY_RATIO_2X        = 2;
+   PE::TValue ODO_INTERVAL_1000MS            = 1.000;
+   PE::TValue ODO_INTERVAL_HYSTERESIS_50MS   = 0.050;
+   PE::TValue ODO_MIN                        = 0;
+   PE::TValue ODO_MAX                        = 4000;
+   PE::COdometerEx odo( SPEED_INTERVAL_100MS,
+                        SPEED_INTERVAL_HYSTERESIS_10MS,
+                        SPEED_MIN,
+                        SPEED_MAX,
+                        SPEED_ACCURACY_RATIO_2X,
+                        ODO_INTERVAL_1000MS,
+                        ODO_INTERVAL_HYSTERESIS_50MS,
+                        ODO_MIN,
+                        ODO_MAX);
+
+   //init values have to be NaN
+   EXPECT_TRUE( PE::isnan(odo.GetSenValue()) );
+
+   //check valid flag
+   EXPECT_FALSE( odo.SetSenValue(1.500, 1.000, 2.000, 1000, false) );
+   EXPECT_TRUE ( odo.SetSenValue(1.500, 1.000, 2.000, 1000, true) ); //first valid odo after previouse invalid
+   EXPECT_TRUE ( PE::isnan(odo.GetSenValue()) ); //NaN
+   EXPECT_TRUE ( odo.SetSenValue(1.500, 1.000, 2.000, 2000, true) );
+   EXPECT_NEAR ( 500, odo.GetSenValue(), 0.0001 );
+
+   //check value range
+   EXPECT_FALSE( odo.SetSenValue(1.500, 1.000, 2.000, 4001, true) );
+   EXPECT_FALSE( odo.SetSenValue(1.500, 1.000, 2.000,   -1, true) );
+   EXPECT_TRUE ( odo.SetSenValue(1.500, 1.000, 2.000, 4000, true) ); //first valid odo after previouse invalid
+   EXPECT_TRUE ( PE::isnan(odo.GetSenValue()) ); //NaN
+   EXPECT_TRUE ( odo.SetSenValue(1.500, 1.000, 2.000, 1999, true) );
+   EXPECT_NEAR ( 1000, odo.GetSenValue(), 0.0001 );
+
+   //check intervals
+   EXPECT_FALSE( odo.SetSenValue(1.500, 1.000, 2.051, 2000, true) );
+   EXPECT_FALSE( odo.SetSenValue(1.500, 1.051, 2.000, 2000, true) );
+   EXPECT_TRUE ( odo.SetSenValue(1.500, 1.000, 2.000, 2000, true) ); //first valid odo after previouse invalid
+   EXPECT_TRUE ( PE::isnan(odo.GetSenValue()) ); //NaN
+   EXPECT_TRUE ( odo.SetSenValue(1.500, 1.000, 2.000, 2600, true) );
+   EXPECT_NEAR ( 300, odo.GetSenValue(), 0.0001 );
 }
 
 
